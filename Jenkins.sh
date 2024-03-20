d5f31d708cdbd27d2a1c8559002d7b6c1ce73b75
Arun Prasath <136972301+Arun-foxy@users.noreply.github.com>
2024-03-20 11:42:06 +0530

    script updates
    script updates

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,1 +1,14 @@
-
+#!/bin/bash
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install openjdk-17-jre -y
+url -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo add-apt-repository universe -y
+sudo apt-get install jenkins -y
+sudo service jenkins start
+/var/lib/jenkins/secrets/initialAdminPassword
